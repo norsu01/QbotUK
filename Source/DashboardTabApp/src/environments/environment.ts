64ecadcd881b1870b51bb62b007a8a3901990a26
NiravRawell21 <63332268+NiravRawell21@users.noreply.github.com>
2020-09-25 17:21:56 +0530

    Update environment.ts

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qboteduukunicontoso.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qbotdemouniversity.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "9e4d7ef0-9135-47fc-b4cd-a6cbc485e0bd",
-        clientId: "bdd735c4-91e4-400c-a5ef-651ee712bdea",
+        tenantId: "7fbbd0a2-8b5a-446c-b301-6a6b1a01202a",
+        clientId: "bde5fdb5-1b73-4815-8661-e6c35f9b53ef",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
