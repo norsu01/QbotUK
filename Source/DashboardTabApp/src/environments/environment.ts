e621474edf005b6e0418a38bc39ed4db2d7732ac
norsu01 <72127720+norsu01@users.noreply.github.com>
2020-09-30 15:00:42 +0530

    Update environment.ts
    Please accept these changes for recent App ID created

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://Qbottesting.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qbotdemoformsteams.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "7fbbd0a2-8b5a-446c-b301-6a6b1a01202a",
-        clientId: "0bf84831-de5e-4a32-aa20-1058b31da7b1",
+        clientId: "d17ad5ea-0050-434b-b666-3d4066dcb825",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
